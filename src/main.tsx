13f5970b4fad84b6a876d9f482913edf438d50db
400kvdhulekundane <400kvdhulekundane@gmail.com>
2026-06-08 00:28:35 +0530

    Fix: add error overlay for debugging white screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+window.onerror = function(msg, src, line, col, err) {
+  document.body.innerHTML = `<pre style="color:red;padding:20px;font-size:12px;white-space:pre-wrap;">${msg}\n${src}:${line}\n${err?.stack}</pre>`;
+};
+
+window.onunhandledrejection = function(e) {
+  document.body.innerHTML = `<pre style="color:red;padding:20px;font-size:12px;white-space:pre-wrap;">Unhandled Promise:\n${e.reason}</pre>`;
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
